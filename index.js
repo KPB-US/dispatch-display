7904285669627f109f4526b96481138680bd4707
Marvin Frederickson <marvin.frederickson@gmail.com>
2017-09-22 11:23:44 -0800

    work

---
+++ index.js
@@ -0,0 +1,100 @@
+/** nodejs server code for dispatch display system
+ *
+ * environment variables:
+ *  GOOGLE_DIRECTIONS_API_KEY
+ *  GOOGLE_STATIC_MAPS_API_KEY
+ */
+
+require('dotenv').config();
+
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const bodyParser = require('body-parser');
+const googleMapsClient = require('@google/maps').createClient({
+  key: process.env.GOOGLE_DIRECTIONS_API_KEY,
+  Promise: Promise
+});
+const STATIC_MAP_BASE_URL =
+  'https://maps.googleapis.com/maps/api/staticmap?&maptype=roadmap&zoom=14&scale=2&key=' +
+  process.env.GOOGLE_STATIC_MAPS_API_KEY;
+
+app.use(morgan('combined')); // logging requests to console
+app.use(bodyParser.json()); // https://stackoverflow.com/questions/5710358/how-to-retrieve-post-query-parameters
+
+// serve static files
+app.use('/socket.io', express.static('node_modules/socket.io-client/dist'));
+app.use('/js', express.static('js'));
+app.use('/css', express.static('css'));
+
+// send the main page
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+/**
+ * handle when a user connects or disconnects
+ */
+io.on('connection', function(socket) {
+  console.log('a user connected', socket.conn.remoteAddress);
+  socket.on('disconnect', function(reason) {
+     console.log('user disconnected', socket.conn.remoteAddress, reason);
+   });
+ });
+
+let testCache = {
+  location: null,
+  data: null,
+};
+
+app.post('/incoming', function(req, res) {
+  // todo! get an ack back, see https://socket.io/docs/#sending-and-getting-data-(acknowledgements)
+  let callNumber = req.body.callNumber;
+
+  // data first
+  io.emit('call', {
+    callNumber,
+    response: req.body,
+  });
+
+  // map next
+  if (req.body.location) {
+    if (testCache.location != null && testCache.location === req.body.location) {
+      io.emit('directions', testCache);
+    } else {
+      googleMapsClient.directions({
+        origin: '231 S Binkley St, Soldotna, AK 99669', // 60.4829661,-151.0722942
+        destination: req.body.location,
+        mode: 'driving',
+      }).asPromise()
+        .then((response) => {
+          console.log('google directions api response', response);
+          if (response.json.status === 'OK') {
+            const enc = encodeURIComponent(response.json.routes[0].overview_polyline.points);
+            const data = {
+              callNumber,
+              response,
+              map: STATIC_MAP_BASE_URL + '&path=enc:' + enc,
+            };
+            testCache = {
+              location: req.body.location,
+              data: data,
+            };
+            io.emit('directions', data);
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }
+
+  res.send('OK');
+});
+
+
+http.listen(3000, function() {
+  console.log('listening on *:3000');
+});
---
+++ js/monitor.js
@@ -0,0 +1,127 @@
+/* eslint-env jquery */
+/* globals moment, io */
+
+const OUT_SECS = 90;
+const socket = io();
+let countdownHandle = null;
+let currentCallNumber = '';
+
+/**
+ * handle directions, display route and map
+ *
+ * data from websocket from server:
+ * @param {string} callNumber id of call from 911 system
+ * @param {object} response google directions api results
+ * @param {string} mapUrl google static map url with route highlighted
+ */
+socket.on('directions', function(msg) {
+  const callNumber = msg.callNumber;
+  const data = msg.response;
+  const mapUrl = msg.mapUrl + '&size=';
+
+  if (callNumber !== currentCallNumber) {
+    // directions are for a different call
+    console.log('directions came in for ', callNumber, ' but we are currently on call ', currentCallNumber);
+    return;
+  }
+
+  // size the map to the container and have the browser fetch it
+  const m = $('.map');
+  m.append($('<img src="' + mapUrl + m[0].parentElement.offsetWidth + 'x' + m[0].parentElement.offsetHeight + '"/>'));
+
+  // display estimated travel time
+  if (data.json.routes.length > 0 &&
+    data.json.routes[0].legs.length > 0 &&
+    data.json.routes[0].legs[0].steps.length > 0) {
+    $('.travel-time').text('estimated travel time is ' + data.json.routes[0].legs[0].duration.text);
+  }
+
+  // display route travel directions
+  for (let i = 0; i < data.json.routes[0].legs[0].steps.length; i++) {
+    $('.route ol').append($('<li>')
+      .html(data.json.routes[0].legs[0].steps[i].html_instructions +
+         ' (' + data.json.routes[0].legs[0].steps[i].distance.text + ')'));
+  }
+});
+
+/**
+ * display initial or updated call information
+ *
+ * data from websocket from server
+ * @param {string} callNumber id of call from 911
+ * @param {object} response call data from 911
+ */
+socket.on('call', function(msg) {
+  // $('.incoming-calls').append($('<li>').append($('<pre>').text(JSON.stringify(msg, undefined, 2))));
+  const callNumber = msg.callNumber;
+  const data = msg.response;
+
+  // if this is a new call, reset the timer and on-screen details
+  if (currentCallNumber != callNumber) {
+    currentCallNumber = callNumber;
+    if (countdownHandle != null) {
+      clearInterval(countdownHandle);
+    }
+    reset();
+
+    let countdownStart = Date.now();
+    // display when the call came in to 911 operator
+    let callTime = moment(data.callDateTime, 'MM/DD/YYYY HH:mm:ss');
+    $('.call .time').text('Call came in at ' + callTime.format('h:mm a'));
+    $('.call .elapsed').text(moment.preciseDiff(callTime, moment()) + ' ago');
+    // display the initial countdown value
+    $('.countdown').text(OUT_SECS);
+    countdownHandle = setInterval(function() {
+      const elapsed = Math.floor((Date.now() - countdownStart) / 1000);
+      $('.countdown').text(OUT_SECS - elapsed);
+      $('.call .elapsed').text(moment.preciseDiff(callTime, moment()) + ' ago');
+      if (elapsed >= OUT_SECS) {
+        $('.countdown').addClass('red');
+      }
+
+      // if we timed out past twice our response time, then reset and hide
+      if (elapsed >= OUT_SECS * 2) {
+        $('.container').addClass('hidden');
+        clearInterval(countdownHandle);
+        countdownHandle = null;
+        currentCallNumber = '';
+      }
+    }, 1000);
+  }
+
+  if (data.callType) {
+    $('.call-type').text(data.callType);
+  }
+  if (data.dispatchCode) {
+    $('.dispatch-code').text(data.dispatchCode);
+  }
+
+  if (data.breathing) {
+    $('.breathing').text('BREATHING: ' + data.breathing);
+  }
+  if (data.conscious) {
+    $('.conscious').text('CONSCIOUS: ' + data.conscious);
+  }
+
+  if (data.location) {
+    $('.location').text(data.location);
+  }
+  if (data.venue) {
+    $('.venue').text(data.venue);
+  }
+  if (data.crossStreets) {
+    $('.cross-streets').text(data.crossStreets);
+  }
+});
+
+/**
+ * clear the screen contents of call data, and unhide the container
+ */
+function reset() {
+  $('.field').text('');
+  $('.route').empty();
+  $('.route').append($('<ol>'));
+  $('.map').empty();
+  $('.countdown').removeClass('red');
+  $('.container').removeClass('hidden');
+}
