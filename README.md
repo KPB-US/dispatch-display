d13774cd14410395256874ec63ce650e9027c641
Marvin Frederickson <marvin.frederickson@gmail.com>
2017-09-22 13:46:16 -0800

    updates

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # dispatch-display
 display 911 dispatch details, timer, and map in station
+
+![selection_214](https://user-images.githubusercontent.com/473165/30765332-a2a3af5e-9f9b-11e7-978e-a816690ce51d.png)
+
+Uses the following environment variables:
+- GOOGLE_DIRECTIONS_API_KEY
+- GOOGLE_STATIC_MAPS_API_KEY
+
+## development
+
+1. Git clone the repo
+1. run `npm install`
+1. run `node index.js`
+1. Point your display's browser to http://localhost:3000
+1. Use postman to post to http://localhost:3000/incoming
--- index.js
+++ index.js
@@ -21,6 +21,11 @@ const STATIC_MAP_BASE_URL =
   'https://maps.googleapis.com/maps/api/staticmap?&maptype=roadmap&zoom=14&scale=2&key=' +
   process.env.GOOGLE_STATIC_MAPS_API_KEY;
 
+let testCache = {
+  location: null,
+  data: null,
+};
+
 app.use(morgan('combined')); // logging requests to console
 app.use(bodyParser.json()); // https://stackoverflow.com/questions/5710358/how-to-retrieve-post-query-parameters
 
@@ -44,11 +49,6 @@ io.on('connection', function(socket) {
    });
  });
 
-let testCache = {
-  location: null,
-  data: null,
-};
-
 app.post('/incoming', function(req, res) {
   // todo! get an ack back, see https://socket.io/docs/#sending-and-getting-data-(acknowledgements)
   let callNumber = req.body.callNumber;
@@ -62,7 +62,7 @@ app.post('/incoming', function(req, res) {
   // map next
   if (req.body.location) {
     if (testCache.location != null && testCache.location === req.body.location) {
-      io.emit('directions', testCache);
+      io.emit('directions', testCache.data);
     } else {
       googleMapsClient.directions({
         origin: '231 S Binkley St, Soldotna, AK 99669', // 60.4829661,-151.0722942
@@ -72,11 +72,12 @@ app.post('/incoming', function(req, res) {
         .then((response) => {
           console.log('google directions api response', response);
           if (response.json.status === 'OK') {
+            console.log('sending map info');
             const enc = encodeURIComponent(response.json.routes[0].overview_polyline.points);
             const data = {
               callNumber,
               response,
-              map: STATIC_MAP_BASE_URL + '&path=enc:' + enc,
+              mapUrl: STATIC_MAP_BASE_URL + '&path=enc:' + enc,
             };
             testCache = {
               location: req.body.location,
