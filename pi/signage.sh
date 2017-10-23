33813e3d97f190eb0b85a52609081cd52c57ff27
Marvin Frederickson <marvin.frederickson@gmail.com>
2017-10-23 08:49:46 -0800

    start local http server

--- pi/signage.sh
+++ pi/signage.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+cd /home/signage/online_check
+nohup python -m SimpleHTTPServer 8000 &
+cd /home/signage
 while true; do
   chromium-browser http://localhost:8000/online_check.html --kiosk --disable-infobars --disable-session-crashed-bubble
   sleep 10s
