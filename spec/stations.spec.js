6574ce735257e61633d7f8dbc02c3da6c0dc0e4c
Marvin Frederickson <marvin.frederickson@gmail.com>
2017-11-06 16:24:26 -0900

    refactor stations and areas

--- spec/stations.spec.js
+++ spec/stations.spec.js
@@ -5,25 +5,28 @@ const stations = require('../lib/stations');
 
 // set up our test stations
 stations.STATIONS.splice(0, stations.STATIONS.length);
-Array.prototype.push.apply(stations.STATIONS, [
-  {id: 'MESA', lat: 59.74515, lng: -151.258885, ip_match_regex: /192\.168\.1\.[0-9]+/},
-  {id: 'NSA', lat: 59.74515, lng: -151.258885, ip_match_regex: /192\.168\.3\.[0-9]+/},
+Array.prototype.push.apply(stations.STATIONS, [ /* eslint no-multi-spaces: off */
+  {id: 'MESA1', area: 'MESA', lat: 59.74515, lng: -151.258885, ip_match_regex: /192\.168\.1\.[0-9]+/},
+  {id: 'TEST1', area: 'MESA', lat: 59.74515, lng: -151.258885, ip_match_regex: /192\.168\.8\.1/},
+  {id: 'MESA2', area: 'MESA', lat: 59.74515, lng: -151.258885, ip_match_regex: /192\.168\.2\.[0-9]+/},
+  {id: 'NSA1',  area: 'NSA',  lat: 59.74515, lng: -151.258885, ip_match_regex: /192\.168\.3\.[0-9]+/},
+  {id: 'TEST2', area: 'NSA',  lat: 59.74515, lng: -151.258885, ip_match_regex: /192\.168\.8\.1/},
 ]);
 
 describe('stations', function() {
   it('should find based on id', function() {
-    const station = stations.findStation('MESA');
+    const station = stations.findStation('MESA1');
     expect(station).to.equal(stations.STATIONS[0]);
   });
 
   it('should find based on ip regex', function() {
-    const station = stations.findStationBasedOnIpMatch('192.168.1.1');
-    expect(station).to.equal(stations.STATIONS[0]);
+    const matches = stations.findStationsByIpMatch('192.168.8.1');
+    expect(matches.length).to.equal(2);
   });
 
   it('should not find based on ip regex', function() {
-    const station = stations.findStationBasedOnIpMatch('192.168.2.1');
-    expect(station).to.be.undefined;
+    const matches = stations.findStationsByIpMatch('192.168.9.1');
+    expect(matches.length).to.equal(0);
   });
 });
 
